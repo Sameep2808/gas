349771497d6a1abd9e5b47f4d5ad5ccfc1589be9
Sameep2808 <sameepote2808@gmail.com>
2021-12-13 14:56:59 -0500

    Task 3.15 Cppcheck and Cpplint

--- cpplint.sh
+++ cpplint.sh
@@ -1,1 +1,1 @@
-cpplint $( find . -name \*.h -or -name \*.cpp | grep -vE -e "^./build/" -e "^./vendor/")> results/cpplint_test.txt
+cpplint $( find . -name \*.h -or -name \*.cpp | grep -vE -e "^./build/" -e "^./test/" -e "^./vendor/")> results/cpplint_test.txt
